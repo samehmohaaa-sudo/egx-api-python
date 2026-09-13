bb8fc91ba023a094c17a5eacc2c8365cbaf8fea6
samehmohaaa-sudo <samehmohaaa@gmail.com>
2026-09-13 16:19:34 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,26 @@
+# config.py
+# قاموس تحويل رموز البورصة المصرية إلى الأكواد العالمية لياهو فاينانشال
+
+STOCK_MAP = {
+    'COMI': 'COMI.CA', 
+    'SWDY': 'SWDY.CA', 
+    'ETEL': 'ETEL.CA', 
+    'TMGH': 'TMGH.CA',
+    'EGAL': 'EGAL.CA', 
+    'MFPC': 'MFPC.CA', 
+    'QNBE': 'QNBE.CA', 
+    'ABUK': 'ABUK.CA',
+    'HDBK': 'HDBK.CA', 
+    'EAST': 'EAST.CA', 
+    'ALCN': 'ALCN.CA', 
+    'ORAS': 'ORAS.CA',
+    'EFIH': 'EFIH.CA', 
+    'EMFD': 'EMFD.CA', 
+    'ADIB': 'ADIB.CA', 
+    'FWRY': 'FWRY.CA',
+    'SCTS': 'SCTS.CA', 
+    'ORHD': 'ORHD.CA', 
+    'EFID': 'EFID.CA', 
+    'CANA': 'CANA.CA',
+    'OCDI': 'OCDI.CA',
+}
